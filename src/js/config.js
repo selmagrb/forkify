1ab5814a1519eb190a1d2e82b585eb54f3c25532
selmagrb <selmagharbi94@yahoo.com>
2023-05-26 15:30:30 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '63660e69-4796-444e-bb9a-59950cc924a2';
+export const MODAL_CLOSE_SEC = 2.5;
